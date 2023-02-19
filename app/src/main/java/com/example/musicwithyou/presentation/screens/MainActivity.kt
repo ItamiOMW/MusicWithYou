5f294f153589a4b6313163381723e9d67989563a
Вадим <itamiomw@gmail.com>
2023-02-19 13:27:40 +0300

    SplashScreen and App Icon implemented

--- app/src/main/java/com/example/musicwithyou/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/example/musicwithyou/presentation/screens/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -54,6 +55,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalAnimationApi::class, ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             val isDarkTheme = userSettingsManager.isDarkTheme.collectAsState(initial = false)
             MusicWithYouTheme(darkTheme = isDarkTheme.value) {
